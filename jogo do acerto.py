c49cf4d8a80f82ae1f9fdbe4f3a6a440f9bfa7c3
Tauanpedreira <168440061+Tauanpedreira@users.noreply.github.com>
2024-05-02 15:08:19 -0300

    Update jogo do acerto.py

--- jogo do acerto.py
+++ jogo do acerto.py
@@ -1,26 +1,44 @@
 import time
 import random
-pergunta=str(input('\033[34molá! pronto para iniciar um game divertido comigo ou quer 1 mn?(S/N): \033[m').upper())
-if pergunta=='S':
-    print('\033[34mok! vamos iniciar!!\033[m')
-    time.sleep(1)
-    print('\033[34mpensei ne um número de 0 a 5... tente adivinhar!!\033[m')
-    num=random.randint(0,5)
-    player=int(input('qual número você acha que o computador pensou? '))
-    if player==num:
-        print('\033[32mPARABENS!! você venceu!!!\033[m')
-    else:
-        print('\033[31mque pena... esperava mais de você!\033[m')
-if pergunta=='N':
-    print('\033[32mok! te darei 1 minuto! se aprece!!\033[m')
+print('\033[32molá, seja bem vindo!!')
+time.sleep(2)
+print('quero ver se você é bom ne jogos de acertos!!')
+time.sleep(2)
+sn=str(input('inciar o game ou esperar 1mn ?(S para iniciar, N para esperar 1mn): \033[m')).upper().strip()
+pc=0
+c=1
+pc=random.randint(1,5)
+if sn=='S':
+    print('\033[32mok! vamos iniciar!!')
+    time.sleep(2)
+    player=int(input('pensei ne um número de 1 a 10... qual você acha que é? \033[m'))
+    while not player==pc:
+        if player !=pc:
+            print('\033[32mhmmm...\033[m \033[31merrou, tente mais uma vez!!\033[m')
+        c=c+1
+        time.sleep(2)
+        player=int(input('\033[32mqual você acha que é? \033[m'))
+    if player==pc:
+        if c<3:
+            print('\033[33mnossa!! você é bom mesmo, apenas com {} vezes você já acertou!!\033[m'.format(c))
+        elif c>3:
+            print('\033[31mé... parabéns, mas deu pra perceber que você não tem muita sorte, você teve {} chances!!\033[31m'.format(c))
+elif sn=='N':
+    print('\033[32mblz, esperarei 1mn!!')
     time.sleep(60)
-    print('\033[31mok! acabou o seu tempo, vamos iniciar!!\033[m')
-    time.sleep(1)
-    print('\033[34mpensei ne um número de 0 a 5... tente adivinhar!!\033[m')
-    num2=(random.randint(0,5))
-    player2=int(input('qual número você acha que o computador pensou? '))
-    if player2==num2:
-        print('\033[32mPARABENS!! você venceu!!!\033[m')
-    else:
-        print('\033[31mque pena... esperava mais de você!\033[m')
+    print('OK! acabou o tempo!!\033[m')
+    time.sleep(2)
+    player=int(input('\033[32mpensei ne um número de 1 a 10... qual você acha que é? \033[m'))
+    while not player==pc:
+        print('\033[32mhmmm... \033[31merrou, tente mais uma vez!!\033[m')
+        c=c+1
+        time.sleep(2)
+        player=int(input('\033[32mqual você acha que é? \033[m'))
+    if player==pc:
+        if c<3:
+            print('\033[33mnossa!! você é bom mesmo, apenas com {} vezes você já acertou!!\033[m'.format(c))
+        elif c>3:
+            print('\033[31mé... parabéns, mas deu pra perceber que você não tem muita sorte, você teve {} chances!!\033[m'.format(c))
+else:
+    print('\033[31mpor favor, reinicie o programa e selecione uma das opções a cima!!\033[31m')
 print('\033[31mFim de jogo.\033[m')
